623286db68eb2d29f591bad6d9e09a757d319a8c
MamoruDS <jlchen2015@gmail.com>
2019-05-07 10:57:52 +0900

    fixed child now send returned message to main process, then exit

--- src/child.js
+++ src/child.js
@@ -3,8 +3,10 @@ import jsto from './main_old'
 process.on('message', async msg => {
     if (msg.method === 'save') {
         await jsto.saveJSON(msg.path, msg.obj, msg.pwd)
+        process.send('ok')
     } else if (msg.method === 'load') {
-        await jsto.loadJSON(msg.path, msg.pwd)
+        const res = await jsto.loadJSON(msg.path, msg.pwd)
+        process.send(res)
     }
     process.exit(0)
 })
